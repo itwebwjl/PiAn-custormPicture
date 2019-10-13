d860c88fad98e943e8a638d577f4ac301919487e
fengtianhe <545704061@qq.com>
2019-10-13 11:27:00 +0800

    集团合作页面 - echart

--- src/page/_cooperation/overview.vue
+++ src/page/_cooperation/overview.vue
@@ -5,6 +5,9 @@
         <div class="total-title">持有产品数量 <span class="tip">(比上月 <span class="red">+1</span>)</span></div>
         <div class="tip">共计: <span class="red">106</span></div>
       </div>
+      <div class="echart-container">
+        <div id="echart" style="height: 3rem"></div>
+      </div>
     </div>
 
     <!--  业务贡献  -->
@@ -33,8 +36,79 @@
 </template>
 
 <script>
+  // 按需引入 引入 ECharts 主模块
+  const echarts = require("echarts/lib/echarts");
+  // 引入柱状图
+  require("echarts/lib/chart/bar");
+
   export default {
-    name: "overview"
+    name: "overview",
+    data() {
+      return {
+        echartOptions: {
+          title: {},
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'shadow'
+            }
+          },
+          grid: {
+            left: -30,
+            right: 0,
+            bottom: 0,
+            top: 15,
+            containLabel: true
+          },
+          xAxis: {
+            show: false,
+            type: 'value',
+            boundaryGap: [0, 0.01]
+          },
+          yAxis: {
+            type: 'category',
+            data: ['好医生', '不动产', '基金', '资管', '健康险', '证券', '信托', '租赁', '养老险', '产险', '银行'],
+            axisLabel: {
+              align: 'left',
+              color: '#666',
+              margin: 50
+            },
+            axisLine: {
+              show: false
+            },
+            axisTick: {
+              show: false
+            }
+          },
+          series: [
+            {
+              type: 'bar',
+              data: [8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 20],
+              barWidth: '15px',
+              barCategoryGap: '10px',
+              label: {
+                show: true,
+                position: 'right',
+                color: '#999'
+              },
+              itemStyle: {
+                color: new echarts.graphic.LinearGradient(1, 0, 0, 0, [{
+                  offset: 0,
+                  color: "#CBE5FF" // 0% 处的颜色
+                }, {
+                  offset: 1,
+                  color: "#63ACFF" // 60% 处的颜色
+                }], false)
+              }
+            }
+          ]
+        }
+      }
+    },
+    mounted() {
+      const echart = echarts.init(document.getElementById('echart'))
+      echart.setOption(this.echartOptions)
+    }
   }
 </script>
 
@@ -65,6 +139,10 @@
           color: #333;
           font-weight: bold;
         }
+      }
+
+      .echart-container {
+        height: 2.8rem;
       }
 
       .row {
